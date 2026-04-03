bbca35ca43ca50f68f3f2400296f64562405fd9d
wordpinchh <wordpinchh001@gmail.com>
2026-04-03 11:45:27 +0530

    link images on share done task 10

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Metadata } from "next";
 import { useState, useEffect } from "react";
 import ContactLeftPanel from "@/components/contact/ContactLeftPanel";
--- src/app/industries/page.tsx
+++ src/app/industries/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Metadata } from "next";
 import { useEffect, useState } from "react";
 import IndustriesHero from "@/components/industries/IndustriesHero";
