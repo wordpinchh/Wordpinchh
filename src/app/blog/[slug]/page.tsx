cd496a36087700b473f9145618adafccad37c893
wordpinchh <wordpinchh001@gmail.com>
2026-05-16 21:58:25 +0530

    Add og:image and twitter:image to fix incomplete Open Graph tags on blog posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,6 +17,9 @@ export async function generateMetadata(
     };
   }
 
+  const postUrl = `https://www.wordpinchh.org/blog/${post.slug}`;
+  const ogImage = "https://www.wordpinchh.org/og-image.png";
+
   return {
     title: post.seoTitle,
     description: post.metaDesc,
@@ -24,19 +27,28 @@ export async function generateMetadata(
     openGraph: {
       title: post.seoTitle,
       description: post.metaDesc,
-      url: `https://www.wordpinchh.org/blog/${post.slug}`,
+      url: postUrl,
       siteName: "Wordpinchh",
       type: "article",
       publishedTime: post.date,
       authors: [post.author],
+      images: [
+        {
+          url: ogImage,
+          width: 1200,
+          height: 630,
+          alt: post.seoTitle,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title: post.seoTitle,
       description: post.metaDesc,
+      images: [ogImage],
     },
     alternates: {
-      canonical: `https://www.wordpinchh.org/blog/${post.slug}`,
+      canonical: postUrl,
     },
   };
 }
